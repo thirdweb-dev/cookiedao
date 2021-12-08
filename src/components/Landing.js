75fcafc745d6fac2191c3c1de5f94fa866dbf86b
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-12-08 14:29:10 +0100

    Fix title

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,7 @@ const Landing = () => {
   return (
     <div className="landing">
       <div className="landing__text">
-        <h1>CookieDao</h1>
+        <h1>CookieDAO</h1>
         <h2>The best DAO for cookie lovers</h2>
         <p>Join thousand other developers in the best DAO for JavaScript developers.</p>
         <div>
