1caf133ddbb6528b0bc8d55a405cb8072b6f5707
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-12-08 16:41:46 +0100

    Create cookie game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,60 @@
-export const Game = () => {
+import { useEffect, useState } from "react";
+import chocolateCookie from "../assets/chocolate-cookie.png";
+
+export const Game = ({ setOpen }) => {
+  const [time, setTime] = useState(5);
+  const [gameOver, setGameOver] = useState(false);
+  const [cookiePosition, setCookiePosition] = useState({ top: "15%", left: "50%" });
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe = setInterval(() => {
+      setTime(time => time > 0 ? time - 1 : 0);
+    }, 1000);
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    if (time === 0) setGameOver(true);
+  }, [time]);
+
+  useEffect(() => {
+    if (gameOver) {
+      setCookiePosition({ display: 'none' });
+    }
+  }, [gameOver]);
+
+  const handleClick = () => {
+    setScore(score => score + 1);
+    setCookiePosition({
+      top: `${Math.floor(Math.random() * 90)}%`,
+      left: `${Math.floor(Math.random() * 90)}%`
+    });
+  };
+
+  const redeemCookies = () => {
+    setOpen(false);
+  };
+
   return (
-    <>
+    <div className="game">
       <div className="overlay" />
       <div className="game-container">
-        game
+        <img src={chocolateCookie} alt="Chocolate Cookie" style={{ ...cookiePosition }} onClick={handleClick} />
+        {gameOver ?
+          <div className="game-over">
+            <h2>Game over!</h2>
+            <p>You clicked {score} cookies!</p>
+            <p>You can now redeem that for {score} $COOKIE tokens</p>
+            <button className="btn-black" onClick={redeemCookies}>Redeem</button>
+          </div>
+          :
+          <>
+            <h2 className="score">{score} points</h2>
+            <h2 className="timer">Time left: {time}s</h2>
+          </>
+        }
       </div>
-    </>
+    </div>
   );
-}
+};
--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -6,17 +6,15 @@ export const GameScreen = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <div className="game">
-          <h1>Cookie Clicker</h1>
-          <h2>Earn <span className="color-cookie">$COOKIE</span> by clicking the cookies
-            that will appear on the screen.</h2>
-          <button className="btn-black" onClick={() => setOpen(!open)}>Play</button>
-        </div>
-        <img className="decoration" src={decoration} alt="Cookie decoration" />
+    <div>
+      <div className="game-screen">
+        <h1>Cookie Clicker</h1>
+        <h2>Earn <span className="color-cookie">$COOKIE</span> by clicking the cookies
+          that will appear on the screen.</h2>
+        <button className="btn-black" onClick={() => setOpen(!open)}>Play</button>
       </div>
-      {open && <Game />}
-    </>
+      <img className="decoration" src={decoration} alt="Cookie decoration" />
+      {open && <Game setOpen={setOpen} />}
+    </div>
   );
 };
