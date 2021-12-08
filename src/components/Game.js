13199ed1abc84553736b0b2da3d02f9aea2fefa7
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-12-08 14:28:19 +0100

    Create game screen

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,15 @@
+import decoration from "../assets/decoration.png";
+
 const Game = () => {
   return (
     <div>
-      game
+      <div className="game">
+        <h1>Cookie Clicker</h1>
+        <h2>Earn <span className="color-cookie">$COOKIE</span> by clicking the cookies
+          that will appear on the screen.</h2>
+        <button className="btn-black">Click</button>
+      </div>
+      <img className="decoration" src={decoration} alt="Cookie decoration" />
     </div>
   );
 };
